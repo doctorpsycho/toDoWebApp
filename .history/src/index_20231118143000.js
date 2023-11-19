8e688050cefda17c953283168da66e88d58e544d
Farhaad Bukhari <farhaadbukhari0@gmail.com>
2023-11-19 22:15:38 +0500

    add files to stage

--- .history/src/index_20231118143000.js
+++ .history/src/index_20231118143000.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get("/" , function( req , res ){
     // res.sendFile(__dirname + ("/index.html"));
     var today = new Date();
-    if (today.getDay() === 6 || today.getDay() === 0 ) {
+    if (today.getDate === 4 || today.getDate === 5) {
         res.send("WeekEnd");
     } else {
         res.send("Workday");
